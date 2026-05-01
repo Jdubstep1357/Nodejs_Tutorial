69eedb817affdd6b7a17b17d958e9a898aa17a01
John Lyons <jwlyons2015@gmail.com>
2026-05-01 12:34:22 -0400

    Installed package module and also got git to work

--- app.js
+++ app.js
@@ -1,15 +1,22 @@
-// Class - Properties and behaviors
-const EventEmitter = require('events');
+const http = require('http');
 
+// event emitter
+const server = http.createServer((req, res) => {
+    if (req.url === '/') {
+        res.write('Hello World');
+        res.end();
+    }
 
-const Logger = require('./logger');
-const logger = new Logger();
+    if (req.url === '/api/courses') {
+        // converts array to string and writes it to response
+        res.write(JSON.stringify([2,3,4]));
+        res.end();
 
-
-// Register a listener - passed data from raised event
-logger.on('messageLogged', (arg) => { // e, eventArg
-    console.log('Listener called', arg);
+        // After running localhost:3000/api/courses will display array
+    }
 });
 
 
-logger.log('message');
+server.listen(3000);
+
+console.log('Listening on port 3000');
