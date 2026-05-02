5118216d9129c8ea3cc23f8857c532db5df22285
John Lyons <jwlyons2015@gmail.com>
2026-05-02 13:33:42 -0400

    Made route parameters

--- express-demo/index.js
+++ express-demo/index.js
@@ -11,6 +11,15 @@ app.get('/api/courses', (req, res) => {
     res.send([1,2,3]);
 });
 
+
+app.get('/api/courses/:id', (req, res) => {
+    res.send(req.params.id);
+});
+
+app.get('/api/posts/:year/:month', (req, res) => {
+    res.send(req.params);
+});
+
 // PORT - an enviornment variable
 const port = process.env.PORT || 3000
 
