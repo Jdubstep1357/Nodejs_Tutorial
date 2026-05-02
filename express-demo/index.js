ab132b5077a61caa6732e6f1809fee1e2a2de3bc
John Lyons <jwlyons2015@gmail.com>
2026-05-02 14:11:02 -0400

    Added in HTTP Get requests and ERROR messages

--- express-demo/index.js
+++ express-demo/index.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const app = express();
 
+const courses = [
+    { id: 1, name: 'course1' },
+    { id: 2, name: 'course2' },
+    { id: 3, name: 'course3' }
+];
+
 // first gets path or URL root of website, 2nd callback function
 app.get('/', (req, res) => {
     res.send('Hello World');
@@ -8,16 +14,14 @@ app.get('/', (req, res) => {
 
 
 app.get('/api/courses', (req, res) => {
-    res.send([1,2,3]);
+    res.send(courses);
 });
 
 
 app.get('/api/courses/:id', (req, res) => {
-    res.send(req.params.id);
-});
-
-app.get('/api/posts/:year/:month', (req, res) => {
-    res.send(req.params);
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    if (!course) res.status(404).send("The course with the given ID was not found");
+    res.send(course);
 });
 
 // PORT - an enviornment variable
