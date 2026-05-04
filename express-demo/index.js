9b77f14c97489065f583283b0550fd6878173b7b
John Lyons <jwlyons2015@gmail.com>
2026-05-03 20:43:10 -0400

    Made ability to be able to delete ids

--- express-demo/index.js
+++ express-demo/index.js
@@ -1,3 +1,5 @@
+// installed from npm
+const Joi = require('joi');
 const express = require('express');
 const app = express();
 
@@ -21,19 +23,61 @@ app.get('/api/courses', (req, res) => {
 });
 
 app.post('/api/courses', (req, res) => {
+
+        const { error } = validateCourse(req.body);
+
+    if (error) {
+        // 400 Bad Request
+        res.status(400).send(error.details[0].message);
+        return;
+    }
+
+
     // Read course object, use properties to create new course object, and add it to course array
     const course = {
         id: courses.length + 1,
-        name: req.body.name,
+        name: req.body.name
     };
     courses.push(course);
     res.send(course);
 });
 
 
+// How to update course
+app.put('/api/courses/:id', (req, res) => {
+
+    // Look up the course
+    // If not existing, return 404
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    if (!course)
+        return res.status(404).send("The course with the given ID was not found");
+    
+
+    const result = validateCourse(req.body);
+    const { error } = validateCourse(req.body); // result.error
+
+    if (error) return res.status(400).send(error.details[0].message);
+    
+
+    // Update course
+    course.name = req.body.name;
+    // Return updated course
+    res.send(course);
+});
+
+
+function validateCourse(course) {
+    const schema = Joi.object({
+        name: Joi.string().min(3).required(),
+    });
+
+    return schema.validate(course);  
+}
+
+
 app.get('/api/courses/:id', (req, res) => {
     const course = courses.find(c => c.id === parseInt(req.params.id));
-    if (!course) res.status(404).send("The course with the given ID was not found");
+    if (!course) return res.status(404).send("The course with the given ID was not found");
     res.send(course);
 });
 
@@ -41,3 +85,17 @@ app.get('/api/courses/:id', (req, res) => {
 const port = process.env.PORT || 3000
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
+
+
+
+// DELTTE
+app.delete('/api/courses/:id', (req, res) => {
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    if (!course) return res.status(404).send("The course with the given ID was not found");
+
+    // Delete
+    const index = courses.indexOf(course);
+    courses.splice(index, 1);
+
+    res.send(course);
+})
