d24008c00ba4d059c37be85a6b1d1267ba5bd793
John Lyons <jwlyons2015@gmail.com>
2026-05-07 11:26:18 -0400

    Patterns for dealing with asynchronous code

--- async-demo/index.js
+++ async-demo/index.js
@@ -1,11 +1,20 @@
-// Synchronous or blocking program
-// 2nd line has to wait for 1st line to finish execution
+
 console.log('Before');
-// Asynchronous. or non blocking function // schedule task to be performed in future
-// displayed LAST after After
-setTimeout(() => {
-    console.log('Reading a user from the database');
-}, 2000);
- 
+const user = getUser(1);
+console.log(user);
 console.log('After');
 
+// WAYS OF EXECUTING WITHOUT WAITING
+// 1. Callbacks
+// 2. Promises
+// 3. Async / Await
+
+function getUser(id) {
+    // does not show id due to it executing AFTER normally called
+setTimeout(() => {
+    console.log('Reading a user from the database');
+    return { id: id, guitHubUsername: 'mosh' };
+}, 2000); 
+
+    return 1;
+}
