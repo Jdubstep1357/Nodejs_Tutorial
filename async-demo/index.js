05aa28862305bad423326bacabadd9d4bcbab746
John Lyons <jwlyons2015@gmail.com>
2026-05-07 11:37:17 -0400

    Callbacks

--- async-demo/index.js
+++ async-demo/index.js
@@ -1,20 +1,33 @@
 
 console.log('Before');
-const user = getUser(1);
-console.log(user);
+getUser(1, (user) => {
+
+    // 2nd argument array of strings
+    getRepositories(user.guitHubUsername, (repos) => {
+        console.log('Repos', repos);
+    });
+});
 console.log('After');
 
-// WAYS OF EXECUTING WITHOUT WAITING
+// WAYS OF EXECUTING or grabbing info
 // 1. Callbacks
 // 2. Promises
 // 3. Async / Await
 
-function getUser(id) {
-    // does not show id due to it executing AFTER normally called
+// callback - function called when result is ready
+
+function getUser(id, callback) {
 setTimeout(() => {
     console.log('Reading a user from the database');
-    return { id: id, guitHubUsername: 'mosh' };
+    callback({ id: id, guitHubUsername: 'mosh' });
 }, 2000); 
 
-    return 1;
+}
+
+function getRepositories(username, callback) {
+    setTimeout(() => {
+        console.log('Calling Github API... ');
+        callback(['repo1', 'repo2', 'repo3']);
+    }, 2000);
+
 }
