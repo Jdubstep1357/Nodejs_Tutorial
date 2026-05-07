22bab91deed4f89afc003a59914b2b39473bb341
John Lyons <jwlyons2015@gmail.com>
2026-05-07 11:41:20 -0400

    Example of callbackhell

--- async-demo/index.js
+++ async-demo/index.js
@@ -1,20 +1,35 @@
+// WAYS OF EXECUTING or grabbing info
+// callback - function called when result is ready
+// 1. Callbacks
+// 2. Promises
+// 3. Async / Await
 
+
+
+// Asynchronous
 console.log('Before');
 getUser(1, (user) => {
 
     // 2nd argument array of strings
     getRepositories(user.guitHubUsername, (repos) => {
-        console.log('Repos', repos);
+        getCommits(repos, (commits) => {\
+            // CALLBACK HELL - Very difficult to read or understand
+
+        });
     });
 });
 console.log('After');
 
-// WAYS OF EXECUTING or grabbing info
-// 1. Callbacks
-// 2. Promises
-// 3. Async / Await
 
-// callback - function called when result is ready
+
+// Synchronous
+console.log('Before');
+const user = getUser(1);
+const repos = getRepositories(user.gitHubUsername);
+const commits = getCommits(repos[0]);
+console.log('After');
+
+
 
 function getUser(id, callback) {
 setTimeout(() => {
