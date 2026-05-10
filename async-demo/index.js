9293feee0b23669329b6277ec64625f1bb485f69
John Lyons <jwlyons2015@gmail.com>
2026-05-10 19:05:50 -0400

    Replaced callbacks with promises

--- async-demo/index.js
+++ async-demo/index.js
@@ -1,48 +1,42 @@
-// WAYS OF EXECUTING or grabbing info
-// callback - function called when result is ready
-// 1. Callbacks
-// 2. Promises
-// 3. Async / Await
-
-
-
 console.log('Before');
-getUser(1, getRepositories);
+getUser(1, (user) => {
+  getRepositories(user.gitHubUsername, (repos) => {
+    getCommits(repos[0], (commits) => {
+      console.log(commits);
+    })
+  })
+});
 console.log('After');
 
-// SOLUTION TO CALLBACK HELL
+function getUser(id) {
+    return new Promise((resolve, reject) => {
+        // Kick off some async work\
 
-// 1st getRrepositories takes a user object
-// 2nd getRepositories takes string and callback function
-function getRepositories(user) {
-    getRepositories(user.guitHubUsername, getCommits);
-}
-
-
-function getCommits(repos) {
-    getCommits(repos, displayCommits);
-}
-
-
-// function gets array of commits and display commits to console
-function displayCommits(commits) {
-    console.log(commits)
-}
-
-
-
-function getUser(id, callback) {
-setTimeout(() => {
-    console.log('Reading a user from the database');
-    callback({ id: id, guitHubUsername: 'mosh' });
-}, 2000); 
-
-}
-
-function getRepositories(username, callback) {
     setTimeout(() => {
-        console.log('Calling Github API... ');
-        callback(['repo1', 'repo2', 'repo3']);
+        console.log('Reading a user from a database...');
+        resolve({ id: id, gitHubUsername: 'mosh' });
     }, 2000);
+
+    });
+
+}
+
+function getRepositories(username) {
+    return new Promise((resolve, reject) => {
+  setTimeout(() => {
+    console.log('Calling GitHub API...');
+    resolve(['repo1', 'repo2', 'repo3']);
+  }, 2000);
+    });
+
+}
+
+function getCommits(repo) {
+    return new Promise((resolve, reject) => {
+    setTimeout(() => {
+        console.log('Calling GitHub API...');
+        resolve(['commit']);
+  }, 2000);
+    });
 
 }
--- async-demo/promise.js
+++ async-demo/promise.js
@@ -1,22 +1,13 @@
 
 const p = new Promise((resolve, reject) => {
-    // kick off some async work
-    // Ex: call database, set timer, call API, Ect
-    // Once done return value
-
-    setTimeout(() => {
-    // resolve(1);
-    reject(new Error('message'))
-
-    }, 2000);
-
+  // Kick off some async work
+  // ...
+  setTimeout(() => {
+    resolve(1); // pending => resolved, fulfilled 
+    reject(new Error('message')); // pending => rejected
+  }, 2000);
 });
 
-
-// consume Promise
-
 p
-    // if successful
-    .then(result => console.log('Result', result))
-    // if error
-    .catch(err => console.log('Error', err.message));
+  .then(result => console.log('Result', result))
+  .catch(err => console.log('Error', err.message));
