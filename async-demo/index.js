5322bc5f7d598dd1c7509943b2858699295c528a
John Lyons <jwlyons2015@gmail.com>
2026-05-10 19:17:05 -0400

    Consuming Promises

--- async-demo/index.js
+++ async-demo/index.js
@@ -1,11 +1,24 @@
 console.log('Before');
-getUser(1, (user) => {
-  getRepositories(user.gitHubUsername, (repos) => {
-    getCommits(repos[0], (commits) => {
-      console.log(commits);
-    })
-  })
-});
+
+// CALLBACK EXAMPLE
+// getUser(1, (user) => {
+//   getRepositories(user.gitHubUsername, (repos) => {
+//     getCommits(repos[0], (commits) => {
+//       console.log(commits);
+//     })
+//   })
+// });
+
+
+
+
+// promises example
+getUser(1)
+    .then(user => getRepositories(user.gitHubUsername))
+    .then(repos => getCommits(repos[0]))
+    .then(commits => console.log('Commits', commits))
+    .catch(err => console.log('Error', err.message));
+
 console.log('After');
 
 function getUser(id) {
