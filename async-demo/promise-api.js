44402549072dc6faa087f6840d4a5cbaaf4dda0d
John Lyons <jwlyons2015@gmail.com>
2026-05-10 19:27:18 -0400

    Running parallel promises working successfully

--- async-demo/promise-api.js
+++ async-demo/promise-api.js
@@ -1,3 +1,18 @@
+const p1 = new Promise((resolve) => {
+    setTimeout(() => {
+        console.log('Async operation 1');
+        resolve(1);
+    }, 2000);
+});
 
-const p = Promise.reject(new Error('reason for rejection... '));
-p.catch(result => console.log(result));
+const p2 = new Promise((resolve) => {
+    setTimeout(() => {
+        console.log('Async operation 2');
+        resolve(2);
+    }, 2000);
+});
+
+
+// After both promises complete, do something
+Promise.all([p1, p2])
+    .then(result => console.log(result));
